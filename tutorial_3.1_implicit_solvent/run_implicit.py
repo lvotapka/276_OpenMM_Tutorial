1c2b8c21fb423c6a6dd6f830ab568f46ecac1b0e
Lane Votapka <lvotapka100@gmail.com>
2022-01-20 13:45:04 -0700

    Added files for tutorial 3

---
+++ tutorial_3.1_implicit_solvent/run_implicit.py
@@ -0,0 +1,40 @@
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from sys import stdout
+
+prmtop = AmberPrmtopFile('alanine_dipeptide.prmtop')
+inpcrd = AmberInpcrdFile('alanine_dipeptide.inpcrd')
+
+# Create the system with an implicit solvent and rigid bonds
+system = prmtop.createSystem(implicitSolvent=OBC2, implicitSolventKappa=1.0/nanometer, constraints=HBonds)
+
+# Create a Langevin integrator for constant temperature.
+integrator = LangevinMiddleIntegrator(300.0*kelvin, 1/picosecond, 0.004*picoseconds)
+
+# Create a simulation object.
+simulation = Simulation(prmtop.topology, system, integrator)
+
+# Uncomment the following line to create a simulation that uses one of the GPUs.
+#platform = Platform.getPlatformByName('CUDA')
+#properties = {'CudaDeviceIndex': '0', 'CudaPrecision': 'mixed'}
+#simulation = Simulation(prmtop.topology, system, integrator, platform, properties)
+
+# Assign atomic positions
+simulation.context.setPositions(inpcrd.positions)
+
+# Set the system's atomic velocities to a random distribution defined by a Maxwell-Boltzmann distribution.
+simulation.context.setVelocitiesToTemperature(300.0*kelvin)
+    
+# Minimize system energy
+simulation.minimizeEnergy()
+
+# Create a reporter to write the trajectory to a file.
+simulation.reporters.append(PDBReporter('alanine_dipeptide_traj.pdb', 1000))
+
+# Create another reporter to display system info as the simulation runs.
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True, volume=True))
+
+# Advance time by 10000 timesteps (40 picoseconds for Langevin integrator).
+simulation.step(10000)
+
---
+++ tutorial_3_analysis/cluster.py
@@ -0,0 +1,66 @@
+"""
+cluster.py
+
+Given an MD trajectory, this script will cluster the trajectory by RMSD
+and write the cluster centroids as PDB files.
+"""
+import mdtraj as md
+import numpy as np
+import scipy.cluster.hierarchy
+from scipy.spatial.distance import squareform
+
+# Define important variables first
+trajectory_file_name = "tryp_benz.pdb"
+align_by_selection = "name CA" # alpha carbons
+clustering_linkage_method = "average" # may also be 'single', 'complete', etc.
+rmsd_cluster_cutoff = 3.75 # in Angstroms
+
+# Load the trajectory file
+traj = md.load(trajectory_file_name)
+
+# Align the trajectory by alpha carbons
+align_indices = traj.topology.select(align_by_selection)
+traj.superpose(traj, atom_indices=align_indices, ref_atom_indices=align_indices)
+
+# Compute all pairwise RMSDs between conformations
+distances = np.empty((traj.n_frames, traj.n_frames))
+for i in range(traj.n_frames):
+    distances[i] = md.rmsd(traj, traj, i)
+print('Max pairwise rmsd: %f nm' % np.max(distances))
+
+# Clustering only accepts reduced form. Squareform's checks are too stringent
+assert np.all(distances - distances.T < 1e-6)
+reduced_distances = squareform(distances, checks=False)
+
+# Implement the average-link clustering
+linkage = scipy.cluster.hierarchy.linkage(
+    reduced_distances, method=clustering_linkage_method)
+
+# Make lists of cluster members
+flattened_clusters = scipy.cluster.hierarchy.fcluster(
+    linkage, rmsd_cluster_cutoff, criterion="distance")
+num_clusters = np.max(flattened_clusters)
+cluster_frames = []
+for cluster_index in range(1, num_clusters+1):
+    cluster_frame_list = []
+    for i, cluster_index2 in enumerate(flattened_clusters):
+        if cluster_index2 == cluster_index:
+            cluster_frame_list.append(i)
+    cluster_frames.append(cluster_frame_list)
+
+# Find the cluster centroids
+for i, cluster in enumerate(cluster_frames):
+    cluster_distances = np.empty((len(cluster), len(cluster)))
+    for j, distance_index in enumerate(cluster):
+        for k, distance_index2 in enumerate(cluster):
+            cluster_distances[j,k] = distances[distance_index, distance_index2]
+            
+    beta = 1
+    index = np.exp(-beta*cluster_distances / cluster_distances.std()).sum(axis=1).argmax()
+    
+    print("cluster:", i, "centroid frame:", cluster[index])
+    
+    # Save centroid structures
+    centroid_name = "tryp_ben_centroid_{}.pdb".format(i)
+    traj[i].save(centroid_name)
+
